adac076b2b4a914d8485298e9ed87774967934fc
ilin.mikhail <lktvdk25@gmail.com>
2018-03-03 11:44:39 +0300

    Примеры учебника (readme)

---
+++ Csharp/Tutor/Lambda/Lambda/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// взял здесь:
+// http://professorweb.ru/my/csharp/charp_theory/level10/10_6.php
+namespace Lambda
+{
+    // Создадим несколько делегатов имитирующих
+    // простейшую форму регистрации
+    delegate int LenghtLogin(string s);
+    delegate bool BoolPassword(string s1, string s2);
+    delegate void Captha(string s1, string s2);
+
+    class Program
+    {
+        private static void SetLogin()
+        {
+            Console.Write("Введите логин: ");
+            string login = Console.ReadLine();
+
+            // Используем лямбда-выражение
+            LenghtLogin lenghtLoginDelegate = s => s.Length;
+
+            int lengthLogin = lenghtLoginDelegate(login);
+            if (lengthLogin > 25)
+            {
+                Console.WriteLine("Слишком длинное имя\n");
+
+                // Рекурсия на этот же метод, чтобы вывести заново логин
+                SetLogin();
+            }
+        }
+        static void Main(string[] args)
+        {
+            SetLogin();
+
+            Console.Write("Введите пароль: ");
+            string password1 = Console.ReadLine();
+            Console.Write("Повторите пароль: ");
+            string password2 = Console.ReadLine();
+
+            // Используем лямбда выражение
+            BoolPassword bp = (s1, s2) => s1 == s2;
+
+            if (bp(password1, password2))
+            {
+                Random ran = new Random();
+                string resCaptha = "";
+                for (int i = 0; i < 10; i++)
+                    resCaptha += (char)ran.Next(0, 100);
+                Console.WriteLine("Введите код xD: " + resCaptha);
+                string resCode = Console.ReadLine();
+
+                // Реализуем блочное лямбда-выражение
+                Captha cp = (s1, s2) =>
+                {
+                    if (s1 == s2)
+                        Console.WriteLine("Регистрация удалась!");
+                    else Console.WriteLine("Не переживай, в следующий раз получится :)");
+                    return;
+                };
+                cp(resCaptha, resCode);
+            }
+            else 
+                Console.WriteLine("Регистрация провалилась. Пароли не совпадают");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Csharp/Tutor/OptionalArguments/OptionalArguments/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Пример использования необязательных аргументов
+// и именованных аргументов
+// взят отсюда
+// http://professorweb.ru/my/csharp/charp_theory/level5/5_10.php
+
+
+namespace OptionalArguments
+{
+    class Program
+    {
+        // Аргументы b и c указывать при вызове необязательно
+        static int mySum(int a, int b = 5, int c = 10)
+        {
+            return a + b + c;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum1 = mySum(3);
+            int sum2 = mySum(3, 12);
+            //использование именованных аргументов
+            int sum3 = mySum(a: 3, b: 10);
+
+            Console.WriteLine("Sum1 = " + sum1);
+            Console.WriteLine("Sum2 = " + sum2);
+            Console.WriteLine("Sum3 = " + sum2);
+
+            Console.ReadLine();
+        }
+    }
+}
+ 
